d1fe4b2b46d1925dfd9750c056dd449902aed8cf
Simona Boldur <simonaboldur@Simonas-MacBook-Pro.local>
2020-05-10 19:51:30 +0200

    add skills component

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SKILL_ITEMS} from "../../data/skills.data";
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-
+  skillItems = SKILL_ITEMS;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/data/skills.data.ts
@@ -0,0 +1,79 @@
+import {Skill} from "../components/skills/skills.model";
+
+export const SKILL_ITEMS: Skill[] = [
+  {
+    name: 'Java 8'
+  },
+  {
+    name: 'Spring'
+  },
+  {
+    name: 'Spring Boot'
+  },
+  {
+    name: 'A​WS'
+  },
+  {
+    name: 'Hibernate'
+  },
+  {
+    name: 'Docker'
+  },
+  {
+    name: 'Kubernetes'
+  },
+  {
+    name: 'Spark'
+  },
+  {
+    name: 'MongoDB'
+  },
+  {
+    name: 'Cassandra'
+  },
+  {
+    name: 'PostgreSql'
+  },
+  {
+    name: 'JUnit'
+  },
+  {
+    name: 'Mockito'
+  },
+  {
+    name: 'C​ucumber'
+  },
+  {
+    name: 'Jenkins'
+  },
+  {
+    name: 'GitlabCI'
+  },
+  {
+    name: '​CircleCi'
+  },
+  {
+    name: 'Agile Methodologies'
+  },
+  {
+    name: 'Git'
+  },
+  {
+    name: 'Zookeeper'
+  },
+  {
+    name: 'Kibana'
+  },
+  {
+    name: 'IntelliJIdea'
+  },
+  {
+    name: 'Github'
+  },
+  {
+    name: 'Bitbucket'
+  },
+  {
+    name: 'Gitlab'
+  }
+]
