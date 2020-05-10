e532ed2e28f029205c93b137f32b7aa086218d9d
Simona Boldur <simonaboldur@Simonas-MacBook-Pro.local>
2020-05-10 19:49:27 +0200

    add experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Experience} from "./experience.model";
+import * as experiences from "../../data/experience.data";
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-
-  constructor() { }
+  experienceItems = experiences.EXPERIENCE_ITEMS;
+  constructor() {
+  }
 
   ngOnInit() {
   }
---
+++ src/app/components/experience/experience.model.ts
@@ -0,0 +1,8 @@
+export interface Experience {
+  title: string;
+  location: string;
+  dateFrom: string;
+  dateTo: string;
+  workResponsibilities?: string[];
+  description?: string [];
+}
