95595845db143e04ae5d2e7615aae8e46900c74f
Simona Boldur <simonaboldur@Simonas-MacBook-Pro.local>
2020-05-10 22:30:58 +0200

    adjust default path, update a lighter profile picture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import {HomeComponent} from "./components/home/home.component";
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: '**', redirectTo: '/home'}
 ];
 
